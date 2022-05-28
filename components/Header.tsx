3c1ed0d8f4cfc8fc35ffec2641e1806631dbebd8
Victor Merino <victor.merinogarcia@gmail.com>
2022-05-28 20:49:59 +0200

    Add some basic styles to Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,23 @@
 import { StyleSheet, Text, View } from 'react-native'
 
+const styles = StyleSheet.create({
+  container: {
+    marginHorizontal: 25,
+  },
+  lightHeader: {
+    fontSize: 35,
+    marginTop: 60,
+  },
+  boldHeader: {
+    fontSize: 40,
+    fontWeight: 'bold',
+  },
+})
 const Header = () => {
   return (
-    <View>
-      <Text>Grab your</Text>
-      <Text>Delicious meal</Text>
+    <View style={styles.container}>
+      <Text style={styles.lightHeader}>Grab your</Text>
+      <Text style={styles.boldHeader}>Delicious meal</Text>
     </View>
   )
 }
