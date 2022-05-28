a142e0f83fea5b442ee592e865cc2017cd711306
Victor Merino <victor.merinogarcia@gmail.com>
2022-05-28 23:39:57 +0200

    Iterate an array instead of duplicate the element: it's bloody easyyyy :)

--- App.tsx
+++ App.tsx
@@ -1,9 +1,35 @@
 import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
+import { FlatList, StyleSheet, Text, View } from 'react-native'
 import CategoryItem from './src/components/CategoryItem'
 import Header from './src/components/Header'
 import Search from './src/components/Search'
 
+const commonCategories = [
+  {
+    name: 'burger',
+    image: require(`./src/assets/images/burger.png`),
+  },
+  {
+    name: 'pizza',
+    image: require(`./src/assets/images/pizza.png`),
+  },
+  {
+    name: 'pasta',
+    image: require(`./src/assets/images/pasta.png`),
+  },
+  {
+    name: 'steak',
+    image: require(`./src/assets/images/steak.png`),
+  },
+  {
+    name: 'cake',
+    image: require(`./src/assets/images/cake.png`),
+  },
+  {
+    name: 'smoothies',
+    image: require(`./src/assets/images/smoothies.png`),
+  },
+]
 const styles = StyleSheet.create({
   container: {
     /* flex: 1,
@@ -18,25 +44,11 @@ const App = () => {
     <View style={styles.container}>
       <Header />
       <Search />
-      <CategoryItem
-        name="burger"
-        image={require(`./src/assets/images/burger.png`)}
-      />
-      <CategoryItem
-        name="pizza"
-        image={require(`./src/assets/images/pizza.png`)}
-      />
-      <CategoryItem
-        name="steak"
-        image={require(`./src/assets/images/steak.png`)}
-      />
-      <CategoryItem
-        name="cake"
-        image={require(`./src/assets/images/cake.png`)}
-      />
-      <CategoryItem
-        name="smoothies"
-        image={require(`./src/assets/images/smoothies.png`)}
+      <FlatList
+        data={commonCategories}
+        renderItem={({ item }) => (
+          <CategoryItem name={item.name} image={item.image} />
+        )}
       />
       <StatusBar />
     </View>
