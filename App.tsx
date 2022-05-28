cfa60d1fa585c7a6684c060ffc10355e3117a61c
Victor Merino <victor.merinogarcia@gmail.com>
2022-05-28 20:16:13 +0200

    Just checked the boilerplate

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { StatusBar } from 'expo-status-bar'
+/* import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View } from 'react-native'
 
 export default function App() {
@@ -18,3 +18,27 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 })
+ */
+
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#ececec',
+  },
+})
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text>This is just a fukin Jelou Güorl</Text>
+      <StatusBar />
+    </View>
+  )
+}
+
+export default App
