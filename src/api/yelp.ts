e1465b465341eb7efe2901732d085f805098cf71
Victor Merino <victor.merinogarcia@gmail.com>
2022-05-31 00:37:44 +0200

    __

--- src/api/yelp.ts
+++ src/api/yelp.ts
@@ -1,11 +1,8 @@
 import axios from 'axios'
-import getEnvVars from '../environment'
-// const { apiKey } = getEnvVars()
 
 import { REACT_APP_YELP_APIKEY } from '@env'
 
 const baseURL = 'https://api.yelp.com/v3/businesses'
-// const apiKey = process.env.REACT_APP_YELP_APIKEY
 
 console.log('apiKey: ' + REACT_APP_YELP_APIKEY)
 export default axios.create({
