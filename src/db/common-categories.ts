34c4c296d635979b23cfaba0c80258bd82461688
Victor Merino <victor.merinogarcia@gmail.com>
2022-05-30 21:53:50 +0200

    Fix paths

--- src/db/common-categories.ts
+++ src/db/common-categories.ts
@@ -1,26 +1,26 @@
 export const commonCategories = [
   {
     name: 'burger',
-    image: require(`./src/assets/images/burger.png`),
+    image: require(`../assets/images/burger.png`),
   },
   {
     name: 'pizza',
-    image: require(`./src/assets/images/pizza.png`),
+    image: require(`../assets/images/pizza.png`),
   },
   {
     name: 'pasta',
-    image: require(`./src/assets/images/pasta.png`),
+    image: require(`../assets/images/pasta.png`),
   },
   {
     name: 'steak',
-    image: require(`./src/assets/images/steak.png`),
+    image: require(`../assets/images/steak.png`),
   },
   {
     name: 'cake',
-    image: require(`./src/assets/images/cake.png`),
+    image: require(`../assets/images/cake.png`),
   },
   {
     name: 'smoothies',
-    image: require(`./src/assets/images/smoothies.png`),
+    image: require(`../assets/images/smoothies.png`),
   },
 ]
