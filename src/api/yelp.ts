34a9d4f61ada0544e4b56b83ac4e9e1fb3cae756
Victor Merino <5514514+VictorMerino@users.noreply.github.com>
2022-06-17 21:10:40 +0200

    Render the actual content from endpoint

--- src/api/yelp.ts
+++ src/api/yelp.ts
@@ -4,7 +4,7 @@ import { REACT_APP_YELP_APIKEY } from '@env'
 
 const baseURL = 'https://api.yelp.com/v3/businesses'
 
-console.log('apiKey: ' + REACT_APP_YELP_APIKEY)
+// console.log('apiKey: ' + REACT_APP_YELP_APIKEY)
 export default axios.create({
   baseURL,
   headers: {
--- src/screens/RestaurantScreen.tsx
+++ src/screens/RestaurantScreen.tsx
@@ -1,21 +1,35 @@
 import { useEffect, useState } from 'react'
-import { Text, View } from 'react-native'
+import { Image, Text, View } from 'react-native'
+import { FlatList } from 'react-native-gesture-handler'
 import useRestaurant from '../hooks/useRestaurant'
 
 const RestaurantScreen = ({ navigation }) => {
   const [{ data, loading, error }, searchRestaurant] = useRestaurant()
   const id = navigation.getParam('id')
-
+  console.log('id', id)
   useEffect(() => {
     searchRestaurant(id)
   }, [])
 
-  console.log({ data, loading, error })
-  return (
-    <View>
-      <Text>Restaurant {id}</Text>
-    </View>
-  )
+  if (loading) return <Text>Loading...</Text>
+
+  if (error) return <Text>{error}</Text>
+
+  if (data && data.photos) {
+    return (
+      <View>
+        <FlatList
+          data={data.photos}
+          keyExtractor={photo => photo}
+          renderItem={({ item }) => (
+            <Image source={{ uri: item }} style={{ height: 200, width: 200 }} />
+          )}
+        />
+        <Text>Restaurant {id}</Text>
+      </View>
+    )
+  }
+  return null
 }
 
 export default RestaurantScreen
