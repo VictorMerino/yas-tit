c556da9d3a15c88885efea145b8da3305ebccf44
Victor Merino <victor.merinogarcia@gmail.com>
2022-06-17 19:40:18 +0200

    Add navigation plugins

--- App.tsx
+++ App.tsx
@@ -1,31 +1,18 @@
-import { StatusBar } from 'expo-status-bar'
-import { useState } from 'react'
-import { View } from 'react-native'
-import Categories from './src/components/Categories'
+import 'react-native-gesture-handler'
+import { createAppContainer } from 'react-navigation'
+import { createStackNavigator } from 'react-navigation-stack'
+import HomeScreen from './src/screens/HomeScreen'
 
-import Header from './src/components/Header'
-import Restaurants from './src/components/Restaurants'
-import Search from './src/components/Search'
-import { commonCategories } from './src/db/common-categories'
+const navigator = createStackNavigator(
+  {
+    Home: HomeScreen,
+  },
+  {
+    initialRouteName: 'Home',
+    defaultNavigationOptions: {
+      title: 'BussinessSearch',
+    },
+  }
+)
 
-const App = () => {
-  const [activeItem, setActiveItem] = useState('Burguer')
-  return (
-    <View>
-      <Header />
-      <Search
-        setTerm={(term: string) => setActiveItem(term)}
-        activeItem={activeItem}
-      />
-      <Categories
-        categories={commonCategories}
-        activeItem={activeItem}
-        setCategory={setActiveItem}
-      />
-      <Restaurants term={activeItem} />
-      <StatusBar />
-    </View>
-  )
-}
-
-export default App
+export default createAppContainer(navigator)
